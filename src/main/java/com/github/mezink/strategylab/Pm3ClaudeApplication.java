7cfc60c2945c7b5549f46e3d636921c71bbd9c61
MezinK <mezinkocahal@hotmail.com>
2026-02-17 22:11:18 +0100

    initial commit

---
+++ src/main/java/com/github/mezink/strategylab/Pm3ClaudeApplication.java
@@ -0,0 +1,13 @@
+package com.github.mezink.strategylab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pm3ClaudeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pm3ClaudeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/mezink/strategylab/Pm3ClaudeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mezink.strategylab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pm3ClaudeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
